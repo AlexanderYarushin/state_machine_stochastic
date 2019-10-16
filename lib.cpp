b71df6fc229bc2cbe5e80a67a5d9d1993a0d9bf9
GustavFr <sasha-yarushin@mail.ru>
2019-10-16 13:57:21 +0300

    Initial commit

---
+++ lib.cpp
@@ -0,0 +1,8 @@
+#include <QTextStream>
+#include <QTextCodec>
+
+void log(const QString text){
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("IBM 866"));
+    QTextStream cout(stdout);
+    cout << text;
+}
