8108601584ceb89d5b32a076484b2a05b3a734b6
GustavFr <sasha-yarushin@mail.ru>
2019-10-16 20:44:44 +0400

    ---

--- lib.cpp
+++
@@ -1,7 +0,0 @@
-#include <QTextStream>
-#include <QTextCodec>
-void log(const QString text){
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("IBM 866"));
-    QTextStream cout(stdout);
-    cout << text;
-}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <QTextStream>
 #include <QTextCodec>
+#include <QTime>
 
 #include "lib.cpp"
 
@@ -11,7 +12,7 @@ void log(const QString text){
     cout << text;
 }
 
-static std::vector<std::vector<QString>> arrayInputOutput  = {
+static std::vector<std::vector<QString>> arrayInputOutput{
     {"Пойти на экзамен",""},                            //1
     {"Попытаться сесть вперед",""},                     //2
     {"Попытаться сесть назад",""},                      //3
@@ -21,52 +22,59 @@ static std::vector<std::vector<QString>> arrayInputOutput  = {
     {"При ответе попытаться подглядеть в телефоне",""}, //7
     {"Попросить преподавателя о тройке",""},            //8
     {"Закончить ответ",""},                             //9
+    {"Выйти из аудитории",""},                             //9
 };
 
-static std::vector<QString> arrayCondition = {
+static std::vector<QString> arrayCondition{
     {"Начало"},                             //1
     {"Прибытие на экзамен"},                //2
     {"Усаживание на переднюю парту"},       //3
     {"Усаживание на последнюю парту"},      //4
     {"Доставание шпаргалки"},               //5
     {"Списывание"},                         //6
-    {"Ответ по билету"},                    //6
-    {"Подглядывание в телефон"},            //7
-    {"Упрашивание преподавателя о тройке"}, //8
-    {"Экзамен сдан"},                       //9
-    {"Экзамен не сдан"}                     //10
+    {"Ответ по билету"},                    //7
+    {"Подглядывание в телефон"},            //8
+    {"Упрашивание преподавателя о тройке"}, //9
+    {"Экзамен сдан"},                       //10
+    {"Экзамен не сдан"}                     //11
 };
 
-static std::vector<std::vector<std::vector<float>>> jumpTable = {
-    {{2,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //1
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //2
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //3
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //4
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //5
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //6
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //7
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //8
-    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},  //9
+static std::vector<std::vector<std::vector<double>>> jumpTable {
+    {{2,1},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},          //1
+    {{0,0},{3,0.7},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},        //2
+    {{0,0},{4,0.3},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},        //3
+    {{0,0},{0,0},{5,0.2},{5,0.3},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},      //4
+    {{0,0},{0,0},{0,0},{0,0},{6,0.9},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}},        //5
+    {{0,0},{0,0},{7,0.8},{7,0.7},{0,0},{7,0.9},{0,0},{0,0},{0,0},{0,0},{0,0}},    //6
+    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{8,0.1},{0,0},{0,0},{0,0},{0,0}},        //7
+    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{9,0.6},{9,0.2},{0,0},{0,0},{0,0}},      //8
+    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{10,0.9},{10,0.3},{10,0.9},{0,0},{0,0}}, //9
+    {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{1,1},{1,1}},          //10
 };
 
 static QTextStream cin(stdin);
 
 void jump(unsigned index){
+    srand(time(nullptr));
+    double rnd = (rand() % 100);
+    rnd /= 100;
     int select = 0, currentSelect = 0;
     unsigned newCondition;
-    std::vector<float> selectArray;
+    std::vector<double> selectArray;
+    std::vector<double> probability;
 
     log("Текущее состояние: "+arrayCondition[index]+"\n");
     log("Действия:\n");
 
     for(unsigned i = 0; i < jumpTable.size(); ++i){
         if(jumpTable[i][index][0] != 0) {
-            log(QString::number(select+1) + ". " + arrayInputOutput[i][0]+"\n");
+            log(QString::number(select+1) + ". " + arrayInputOutput[i][0]+ " / P = " + QString::number(jumpTable[i][index][1]) + "\n");
             select++;
             selectArray.push_back(jumpTable[i][index][0]);
+            probability.push_back(jumpTable[i][index][1]);
         }
     }
-    repeatEnter:
+repeatEnter:
     log("Выберите действие: ");
     cin >> currentSelect;
 
@@ -77,10 +85,19 @@ void jump(unsigned index){
 
     newCondition = selectArray[currentSelect-1]-1;
 
-    log("\nПереход из состояния \""+arrayCondition[index]+"\" в состояние \""+arrayCondition[newCondition]+"\"\n\n");
+    if(newCondition == 9) if(rnd > 0.5) newCondition = 10;
 
-    log("------------------------------\n\n");
-    jump(newCondition);
+    if(probability[currentSelect-1] > rnd){
+
+        log("\nПереход из состояния \""+arrayCondition[index]+"\" в состояние \""+arrayCondition[newCondition]+"\"\n\n");
+
+        log("------------------------------\n\n");
+
+        jump(newCondition);
+    }else{
+        log("\nПереход из состояния \""+arrayCondition[index]+"\" в состояние \""+arrayCondition[newCondition]+" не удался\"\n\n");
+        jump(index);
+    }
 }
 
 
